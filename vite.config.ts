f4c264896127ac77659216eacb491252a2df6498
zukki30 <kw4869ify@gmail.com>
2025-04-26 00:37:48 +0900

    feat: Add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,17 @@ export default defineConfig({
       name: 'zukki-design-system',
       fileName: (format) => `zukki-design-system.${format}.js`,
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/', 'src/test/'],
+    },
+    exclude: [...configDefaults.exclude, 'e2e/*'],
   },
 });
