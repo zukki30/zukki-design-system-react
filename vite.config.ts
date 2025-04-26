bf29408ae728460eca368a67abb0579fc3154e31
zukki30 <kw4869ify@gmail.com>
2025-04-27 00:17:47 +0900

    feat: Add vanilla-extract packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   build: {
     lib: {
       entry: './src/main.tsx',
