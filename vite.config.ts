befecced2bcb163a8904143bb3b5414591f61041
zukki30 <kw4869ify@gmail.com>
2026-06-07 00:10:40 +0900

    fix: update test setup paths and types for improved compatibility with Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: ['./test/setup.ts'],
     css: true,
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: ['node_modules/', 'src/test/'],
+      exclude: ['node_modules/', 'test/'],
     },
     exclude: [...configDefaults.exclude, 'e2e/*'],
   },
